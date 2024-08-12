4e3204042f207f6f2a23e9ab7784dfa47e54ae60
Newton-E <67238664+Newton-E@users.noreply.github.com>
2024-08-12 20:51:46 +0100

    Add files via upload

---
+++ includes/email_templates/otpr.php
@@ -0,0 +1,70 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Document</title>
+</head>
+
+<body style="-webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6; background-color: #f6f6f6;">
+<table style="background-color: #f6f6f6; width: 100%;">
+    <tr>
+        <td></td>
+        <td style="display: block !important; max-width: 600px !important; margin: 0 auto !important; clear: both !important; width: 100%;">
+            <div style="display: block !important; max-width: 600px !important; margin: 0 auto !important; clear: both !important;">
+                <table style="background: #fff; border: 1px solid #e9e9e9; border-radius: 3px;" width="100%" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="padding: 20px;">
+                            <table  cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td style="font-size: 16px; color: #fff; font-weight: 500; padding: 20px; text-align: center;border-radius: 3px 3px 0 0;background: #297efd;">
+                                        OTP Resend confirmation
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><h2 style="font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: #000; text-align:center;">{EMAIL_TITLE}</h2></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 0 0 20px;">
+                                        <h3 style="font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: #000; margin: 40px 0 0; line-height: 1.2; font-weight: 400;">Hi {TO_NAME}, </h3>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 0 0 20px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; line-height: 1.9em;">
+                                        This is your new verification code
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 0 0 20px; text-align: center;">
+                                        <h1 style="font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; color: #000;">{TO_OTP}</h1>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 0 0 20px; font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; line-height: 1.9em;">
+                                       Best Regards, Inewton Electronic service.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 0 0 20px; text-align: center;">
+                                        <a href="{CUSTOM_URL}" style="font-family: Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif; text-decoration: none; color: #FFF; background-color: #297efd; border: solid #297efd; border-width: 5px 10px; line-height: 2; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize;">Visit site</a>
+                                    </td>
+                                </tr>
+                              </table>
+                        </td>
+                    </tr>
+                </table>
+                <div style="width: 100%; clear: both; color: #999; padding: 20px;">
+                    <table width="100%">
+                        <tr>
+                            <td style="padding: 0 0 20px;  text-align: center;">Contact us <a href="www.isrnewton.com" style="color: #297efd; text-decoration: underline;">www.isrnewton.com</a></td>
+                        </tr>
+                    </table>
+                </div></div>
+        </td>
+        <td></td>
+    </tr>
+</table>
+
+</body>
+</html>
